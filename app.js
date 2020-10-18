f6ca5c3e4bafc3e061461849f128a8f75526896b
atbee <atb@odds.team>
2020-10-18 20:13:39 +0700

    Update packages and clean up dockerfile :bee:

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('local development using nodemon in docker works!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
